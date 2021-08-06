55706701477acca789ccb9b53b8bbc484030e39f
NikitaOtr <nikita137174@list.ru>
2021-08-06 16:26:22 +0300

    init

--- script.js
+++ script.js
@@ -2,27 +2,33 @@
 
 const isNumber = n => !isNaN(parseInt(n)) && isFinite(n);
 
+function startGame (bool) {
+    if (bool) {play();}
+}
+
 function play() {
     const hiddenNumber = Math.round(Math.random() * (100 - 1)  + 1);
+    let turn = 10;
 
     function checkAnswer(message, addition = ''){
         let userNumber = prompt(addition + message);
         if (userNumber === null){
-            alert('Игра окончена');
+            startGame(confirm(`Игра окончена. Хотели бы сыграть ещё?`));
         } else if (!isNumber(userNumber)) {
-            checkAnswer(message, 'Введите число!!! \n');
+            checkAnswer(message, 'Введите число!!!\n');
+        } else if (hiddenNumber === +userNumber) {
+            startGame(confirm(`Поздравляю!!! Вы угадали. Загаданное число ${hiddenNumber}. Хотели бы сыграть ещё?`));
+        } else if (turn === 1) {
+            startGame(confirm('Попытки закончились. Хотели бы сыграть ещё?'));
         } else if (hiddenNumber < +userNumber) {
-            checkAnswer(`Загаданное число меньше чем ${userNumber}.`);
+            turn--;
+            checkAnswer(`Загаданное число меньше чем ${userNumber}. Осталось попыток ${turn}.`);
         } else if (hiddenNumber > +userNumber) {
-            checkAnswer(`Загаданное число больше чем ${userNumber}.`);
-        } else {
-            alert('Поздравляю!!! Вы угадали.');
+            turn--;
+            checkAnswer(`Загаданное число больше чем ${userNumber}. Осталось попыток ${turn}.`);
         }
     }
-    checkAnswer(`Попробуй угадать с первого раза.`);
+    checkAnswer(`Попробуй угадать число. Осталось попыток ${turn}.`);
 }
 
-let begin = confirm('Угадайте число от 1 до 100');
-if (begin) {
-    play();
-}
+startGame(confirm('Попробуйте угадать число от 1 до 100.'));
