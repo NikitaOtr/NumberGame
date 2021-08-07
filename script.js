22d28912995aa9e9b7af4956cfd72952234950df
NikitaOtr <nikita137174@list.ru>
2021-08-07 21:30:08 +0300

    last

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
 'use strict';
 
-const isNumber = n => !isNaN(parseInt(n)) && isFinite(n);
+const isInt = n => Number.isInteger(parseInt(n)) && isFinite(n);
 
-function startGame (bool) {
-    if (bool) {play();}
+function getUserNumber(massage, addition = '') {
+    const userNumber = prompt(addition + massage);
+    if (userNumber === null) {return null;}
+    if (isInt(userNumber)) {return +userNumber;}
+    return getUserNumber(massage, 'Введиет целое число!!!\n');
 }
 
-function play() {
+function startBot (bool) {
+    if (bool) {numberBot();}
+}
+
+function numberBot() {
     const hiddenNumber = Math.round(Math.random() * (100 - 1)  + 1);
     let turn = 10;
 
-    function checkAnswer(message, addition = ''){
-        let userNumber = prompt(addition + message);
+    function checkAnswer(message){
+        let userNumber = getUserNumber(message);
         if (userNumber === null){
-            startGame(confirm(`Игра окончена. Хотели бы сыграть ещё?`));
-        } else if (!isNumber(userNumber)) {
-            checkAnswer(message, 'Введите число!!!\n');
-        } else if (hiddenNumber === +userNumber) {
-            startGame(confirm(`Поздравляю!!! Вы угадали. Загаданное число ${hiddenNumber}. Хотели бы сыграть ещё?`));
+            return startBot(confirm(`Игра окончена. Хотели бы сыграть ещё?`));
+        } else if (hiddenNumber === userNumber) {
+            return startBot(confirm(`Поздравляю!!! Вы угадали. Загаданное число ${hiddenNumber}. Хотели бы сыграть ещё?`));
         } else if (turn === 1) {
-            startGame(confirm('Попытки закончились. Хотели бы сыграть ещё?'));
-        } else if (hiddenNumber < +userNumber) {
+            return startBot(confirm('Попытки закончились. Хотели бы сыграть ещё?'));
+        } else if (hiddenNumber < userNumber) {
             turn--;
-            checkAnswer(`Загаданное число меньше чем ${userNumber}. Осталось попыток ${turn}.`);
+            return checkAnswer(`Загаданное число меньше чем ${userNumber}. Осталось попыток ${turn}.`);
         } else if (hiddenNumber > +userNumber) {
             turn--;
-            checkAnswer(`Загаданное число больше чем ${userNumber}. Осталось попыток ${turn}.`);
+            return checkAnswer(`Загаданное число больше чем ${userNumber}. Осталось попыток ${turn}.`);
         }
     }
-    checkAnswer(`Попробуй угадать число. Осталось попыток ${turn}.`);
+    return checkAnswer(`Попробуй угадать число. Осталось попыток ${turn}.`);
 }
 
-startGame(confirm('Попробуйте угадать число от 1 до 100.'));
+startBot(confirm('Попробуйте угадать число от 1 до 100.'));
